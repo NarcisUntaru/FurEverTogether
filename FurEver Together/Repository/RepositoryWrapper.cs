7307e217fa3eeb4b3c506b344a4060ff4d7c55d2
NarcisUntaru <untaru.narcis.z3f@student.ucv.ro>
2025-11-12 00:59:04 +0200

    Refactorized the repository wrapper to help reduce the cognitive complexity. Extracted logic intro smaller methods.

--- FurEver Together/Repository/RepositoryWrapper.cs
+++ FurEver Together/Repository/RepositoryWrapper.cs
@@ -9,88 +9,25 @@ public class RepositoryWrapper : IRepositoryWrapper
 {
     private readonly FurEverTogetherDbContext _furEverTogetherDbContext;
     private IAdoptionRepository? _adoptionRepository;
-    //private ICatRepository? _catRepository;
     private IContactUsRepository? _contactUsRepository;
     private IPetRepository? _petRepository;
     private IUserRepository? _userRepository;
     private IVolunteerRepository? _volunteerRepository;
 
-    public IAdoptionRepository AdoptionRepository
-    {
-        get
-        {
-            if (_adoptionRepository == null)
-            {
-                _adoptionRepository = new AdoptionRepository(_furEverTogetherDbContext);
-            }
+    public IAdoptionRepository AdoptionRepository =>
+        _adoptionRepository ??= new AdoptionRepository(_furEverTogetherDbContext);
 
-            return _adoptionRepository;
-        }
-    }
+    public IContactUsRepository ContactUsRepository =>
+        _contactUsRepository ??= new ContactUsRepository(_furEverTogetherDbContext);
 
-    //public ICatRepository CatRepository
-    //{
-    //    get
-    //    {
-    //        if (_catRepository == null)
-    //        {
-    //            _catRepository = new CatRepository(_furEverTogetherDbContext);
-    //        }
+    public IPetRepository PetRepository =>
+        _petRepository ??= new PetRepository(_furEverTogetherDbContext);
 
-    //        return _catRepository;
-    //    }
-    //}
+    public IUserRepository UserRepository =>
+        _userRepository ??= new UserRepository(_furEverTogetherDbContext);
 
-    public IContactUsRepository ContactUsRepository
-    {
-        get
-        {
-            if (_contactUsRepository == null)
-            {
-                _contactUsRepository = new ContactUsRepository(_furEverTogetherDbContext);
-            }
-
-            return _contactUsRepository;
-        }
-    }
-
-    public IPetRepository PetRepository
-    {
-        get
-        {
-            if (_petRepository == null)
-            {
-                _petRepository = new PetRepository(_furEverTogetherDbContext);
-            }
-
-            return _petRepository;
-        }
-    }
-
-    public IUserRepository UserRepository
-    {
-        get
-        {
-            if (_userRepository == null)
-            {
-                _userRepository = new UserRepository(_furEverTogetherDbContext);
-            }
-
-            return _userRepository;
-        }
-    }
-    public IVolunteerRepository VolunteerRepository
-    {
-        get
-        {
-            if (_volunteerRepository == null)
-            {
-                _volunteerRepository = new VolunteerRepository(_furEverTogetherDbContext);
-            }
-
-            return _volunteerRepository;
-        }
-    }
+    public IVolunteerRepository VolunteerRepository =>
+        _volunteerRepository ??= new VolunteerRepository(_furEverTogetherDbContext);
 
     public RepositoryWrapper(FurEverTogetherDbContext furEverTogetherDbContext)
     {
@@ -101,9 +38,9 @@ public class RepositoryWrapper : IRepositoryWrapper
     {
         _furEverTogetherDbContext.SaveChanges();
     }
+
     public async Task SaveAsync()
     {
         await _furEverTogetherDbContext.SaveChangesAsync();
     }
-    
 }
