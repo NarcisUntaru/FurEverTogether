7519e0cdf88a74838e99a3cb1a2c119a71f8f175
NarcisUntaru <untaru.narcis.z3f@student.ucv.ro>
2025-11-11 20:02:19 +0200

    Add [Required] attributes to PersonalityProfile properties to enforce model validation (SonarQube - code smell).

--- FurEver Together/DataModels/PersonalityProfile.cs
+++ FurEver Together/DataModels/PersonalityProfile.cs
@@ -1,19 +1,31 @@
 ﻿using FurEver_Together.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace FurEver_Together.DataModels
 {
     public class PersonalityProfile
     {
+        [Required]
         public EnergyLevel EnergyLevel { get; set; }
+        [Required]
         public Sociability Sociability { get; set; }
+        [Required]
         public AffectionLevel AffectionLevel { get; set; }
+        [Required]
         public Trainability Trainability { get; set; }
+        [Required]
         public Playfulness Playfulness { get; set; }
+        [Required]
         public AggressionLevel AggressionLevel { get; set; }
+        [Required]
         public NoiseLevel NoiseLevel { get; set; }
+        [Required]
         public Question GoodWithKids { get; set; }
+        [Required]
         public Question GoodWithOtherPets { get; set; }
+        [Required]
         public Adaptability Adaptability { get; set; }
+        [Required]
         public AnxietyLevel AnxietyLevel { get; set; }
 
         public List<int> ToVector()
